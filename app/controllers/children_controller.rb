71950d8fb4b03efbd5b258eedb58fe861fe203b9
Jennifer Pazos <jmp521@gmail.com>
2016-10-24 20:43:20 +0000

    shows list of children

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,9 +1,28 @@
 class ChildrensController < ApplicationController
 
-  # get '/children'
-  #   erb :index
-  #
-  # end
 
+
+  get '/children/add_child' do
+
+    erb :'/children/add_children'
+
+  end
+  post '/children/add_child' do
+    if is_logged_in?
+      @child = Children.new(:child_name => params[:child][:name])
+
+       if @child.save
+         @parent = Parents.find_by_id(session[:id])
+         @parent_child = ParentsChildren.new()
+
+         @parent_child.child_id = @child.id
+         @parent_child.parent_id = @parent.id
+         @parent_child.save
+         
+
+         redirect to '/parents'
+       end
+    end
+  end
 
 end
--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -4,32 +4,16 @@ class ParentsController < ApplicationController
     if is_logged_in?
       @parent = Parents.find_by_id(session[:id])
 
+      @children = Children.all
+      binding.pry
+
       erb :'/parents/index'
+          # binding.pry
     else
       redirect to '/login'
     end #closes if statement
   end #closes get
 
 
-  post '/parents/add_child' do
-    if is_logged_in?
-      @child = Children.new(:child_name => params[:child][:name])
 
-       if @child.save
-         @parent = Parents.find_by_id(session[:id])
-         @parent_child = ParentsChildren.new()
-
-         @parent_child.child_id = @child.id
-         @parent_child.parent_id = @parent.id
-         @children = Children.all
-         @children.each do |child|
-           if @children.id == @parent_child.child_id
-             @each_child << child
-           end
-           @each_child
-         end
-         redirect to '/parents'
-       end
-    end
-  end
 end #closes class
